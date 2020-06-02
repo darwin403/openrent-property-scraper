906d942a811da559a49aaad33032c234ec337bce
skdcodes <skdcodes@gmail.com>
2020-06-02 09:42:36 +0530

    Fixes excel file writing to append/create

--- scrape.py
+++ scrape.py
@@ -1,31 +1,29 @@
-import requests
+import logging
+from math import ceil, sqrt
+import os
 import re
-from functools import reduce
 from concurrent.futures import ThreadPoolExecutor as PoolExecutor
-import xlsxwriter
+from functools import reduce
+
+import coloredlogs
+import requests
+from utils import excel
+
+coloredlogs.install(
+    fmt='%(asctime)s [%(programname)s] %(levelname)s %(message)s')
+
 
 # threads
 CONCURRENCY = 1
 
-# load pins
-pins = []
-with open('pins.csv') as f:
-    for key, pin in enumerate(f):
-        if key == 0:
+
+# load postcodes
+postcodes = []
+with open('postcodes.txt') as f:
+    for key, postcode in enumerate(f):
+        if not postcode.strip():
             continue
-        if not pin.strip():
-            continue
-        pins.append(pin.strip())
-
-
-# clean ids
-def clean(ids):
-    return [i.strip() for i in ids if i.strip()]
-
-
-# array chunks
-def chunks(l, n):
-    return [l[i:i + n] for i in range(0, len(l), n)]
+        postcodes.append(postcode.strip())
 
 
 # fetch all properties for given ids
@@ -40,7 +38,7 @@ def get_properties(ids):
         r = requests.get(url)
         return r.json()
     except Exception as e:
-        print('Request failed:', e)
+        logging.error('Request failed: %s' % e)
 
     return []
 
@@ -59,65 +57,70 @@ def get_stats(properties):
 
     let_percentage = round((let_count/total_count)*100,
                            2) if let_count != 0 else 0
-    let_total_price = round(
-        reduce(lambda a, b: a + b['rentPerMonth'], let_properties, 0), 2)
+    let_total_price = reduce(
+        lambda a, b: a + b['rentPerMonth'], let_properties, 0)
     let_average_price = round(
         let_total_price/let_count, 2) if let_count != 0 else 0
 
     return (rent_count, let_count, let_percentage, let_average_price)
 
 
-# create new output file
-wb = xlsxwriter.Workbook('openrent.xlsx')
-ws = wb.add_worksheet()
-ws.write_row(0, 0, ('Post Code', 'Total Rent',
-                    'Total Let', '% Let', 'Average Let Price'))
-
-
-# task to get properties for given pin
-def task_properties(pin):
-    row = pins.index(pin) + 1
-    url = 'https://www.openrent.co.uk/properties-to-rent/?term=%s&bedrooms_max=-1' % pin
+# task to get properties for given postcode
+def task_properties(postcode):
+    url = 'https://www.openrent.co.uk/properties-to-rent/?term=%s&bedrooms_max=-1' % postcode
 
     try:
         r = requests.get(url)
     except Exception as e:
-        print('Request failed:', e)
-        ws.write_row(row, 0, (pin, ) + (0,)*4)
-        return (pin, False)
+        logging.error('Request failed: %s' % e)
+        return (False, (postcode,) + (0,)*4)
 
     ids_match = re.search(
         r"PROPERTYIDS = \[(.*?)\];", r.text, re.MULTILINE | re.DOTALL)
 
     if not ids_match:
-        ws.write_row(row, 0, (pin, ) + (0,)*4)
-        print('%s: No property ids founds on: %s' % (pin, url))
-        return (pin, False)
+        logging.warning('%s: No property ids founds on: %s' % (postcode, url))
+        return (False, (postcode,) + (0,)*4)
 
     ids_raw = ids_match.group(1).split(',')
-    ids = clean(ids_raw)
+    ids = [i.strip() for i in ids_raw if i.strip()]
 
     properties = []
 
-    # multi thread fetch
-    with PoolExecutor(max_workers=20) as executor:
-        for properties_chunk in executor.map(get_properties, chunks(ids, 50)):
+    chunk_size = 50
+    chunks = [ids[i:i + chunk_size] for i in range(0, len(ids), chunk_size)]
+
+    # multi threaded pagination
+    with PoolExecutor(max_workers=ceil(sqrt(CONCURRENCY))) as executor:
+        for properties_chunk in executor.map(get_properties, chunks):
             properties += properties_chunk
 
     stats = get_stats(properties)
-    ws.write_row(row, 0, (pin, ) + stats)
-    return (pin, True)
+    return (True, (postcode,) + stats)
 
 
 if __name__ == "__main__":
+    logging.info('bot started.')
     try:
-        # multi thread over all pins
-        with PoolExecutor(max_workers=CONCURRENCY) as executor:
-            for (pin, status) in executor.map(task_properties, pins):
-                print('%s: %s!' % (pin, 'done' if status == True else 'failed'))
+        # multi thread over all postcodes
+        with PoolExecutor(max_workers=ceil(sqrt(CONCURRENCY))) as executor:
+            for (status, row) in executor.map(task_properties, postcodes):
+                excel.insert(row[0], {
+                    "totalRentCount": row[1],
+                    "totalLetCount": row[2],
+                    "totalLetPercentage": row[3],
+                    "totalLetAverage": row[4]
+                })
+                logging.info('[%s]: %s!' %
+                             (row[0], 'done' if status == True else 'failed'))
 
-        wb.close()
     except Exception as e:
-        # save existing data on any error
-        wb.close()
-        print(e)
+        logging.error(e)
+    finally:
+        logging.info('saving to file')
+        excel.save()
+
+        logging.info('bot completed.')
+        
+        # stall program
+        input("Press any key to continue ...")
